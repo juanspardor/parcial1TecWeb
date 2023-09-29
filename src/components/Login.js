a501da07e74c06e3da65d33193cc52eb11bacfce
juanspardor <j.pardor@uniandes.edu.co>
2023-09-29 09:20:13 -0500

    version final

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,33 @@
 import {Form, Row, Col, Card} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 function Login(props) {
-  
+    const [formValues, setFormValues] = useState({nombreUsuario:"", password:""})
+    const [validationStates, setValidationStates] = useState({userState:false, passwordState:false})
+
+    const hangleUserCahnge = ((e) => {
+        const valor = e.target.value
+        setFormValues({...formValues, nombreUsuario: e.target.value})
+        if(valor.length < 5)
+        {
+            setValidationStates({...validationStates, userState: true})
+        }
+      });
+    
+      const handlePasswordChange = ((e) => {
+        const valor =  e.target.value
+        setFormValues({...formValues, password: valor})
+    
+        
+        if(valor.length < 5)
+        {
+            setValidationStates({...validationStates, passwordState: true})
+        }
+    
+      });
+
   return (
     <div>
        <Card>
@@ -11,11 +35,11 @@ function Login(props) {
         <Form>
       <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
         <Form.Label>Nombre de usuario</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
+        <Form.Control type="mb-3" placeholder="Ingresa tu usuario" onChange={hangleUserCahnge} value={formValues.nombreUsuario} isInvalid = {validationStates.userState}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
         <Form.Label>Contraseña</Form.Label>
-        <Form.Control as="textarea" rows={3} />
+        <Form.Control type="mb-3" placeholder="Ingresa tu contraseña" onChange={handlePasswordChange} value={formValues.password} isInvalid = {validationStates.passwordState}/>
       </Form.Group>
     </Form>
     <Row>
@@ -28,6 +52,9 @@ function Login(props) {
             <Button>Cancelar</Button>
         </Col>
     </Row>
+
+
+    {validationStates.passwordState && validationStates.userState && <p>Revisa tus credenciales</p>}
         </Card.Body>
        </Card>
     </div>
